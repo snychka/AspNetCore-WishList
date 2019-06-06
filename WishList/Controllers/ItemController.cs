1bba1e892dc69c0e7d1e77a19b25cc5d31b3c733
snychka <stefannychka@gmail.com>
2019-06-06 16:03:50 -0600

    add soln with proper .csproj

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        // GET: /<controller>/
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        // return View("Index", _context.Items.ToList<Item>());
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        //_context.Add<Item>(item);
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // seems like this'd be slow
+        //_context.Items.Remove(_context.Items.Find(Id));
+        //Item item = (Item)_context.Items.Where<Item>(i => i.Id == Id);
+        public IActionResult Delete(int id)
+        {
+
+            Item item = (Item)_context.Items.FirstOrDefault(i => i.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using WishList.Data;
 
 namespace WishList
 {
@@ -16,15 +17,38 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Run(async (context) =>
+            /*app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
-            });
+            });*/
+
+
+            //            In the Configure method before UseMvcWithDefaultRoute, 
+            //add a condition that checks if env is set to Development 
+            //    using the IsDevelopment() method.
+
+            //If Development, it should call UseDeveloperExceptionPage 
+            //                                    on app to get better detailed error pages.
+
+            //Otherwise, it should call UseExceptionHandler on app and pass it the argument "/Home/Error".
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
